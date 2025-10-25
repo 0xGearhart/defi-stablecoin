0e44fa2c216c252071e35881009d2d7adfacd634
gearhart <geardev26@gmail.com>
2025-10-25 13:41:37 -0700

    DecentralizedStableCoin initial layout and functionality

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -2,4 +2,40 @@
 
 pragma solidity ^0.8.19;
 
-contract DecentralizedStableCoin {}
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__AmountMustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    constructor(
+        address _owner
+    ) ERC20("Decentralized Stable Coin", "DSC") Ownable(_owner) {}
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+}

8cbc5f87977f913c412c0d6ff076b381c300da37
gearhart <geardev26@gmail.com>
2025-10-25 13:45:36 -0700

    unit tests for DecentralizedStableCoin

--- test/unit/DecentralizedStableCoinTest.t.sol
+++ test/unit/DecentralizedStableCoinTest.t.sol
@@ -4,19 +4,123 @@ pragma solidity ^0.8.19;
 import {Test, console} from "forge-std/Test.sol";
 // import {DeployDSC} from "../../script/DeployDSC.s.sol";
 // import {HelperConfig} from "../../script/HelperConfig.s.sol";
-import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DecentralizedStableCoin, Ownable} from "../../src/DecentralizedStableCoin.sol";
 
 contract DecentralizedStableCoinTest is Test {
     DecentralizedStableCoin public dsc;
     // DeployDSC public deployer;
     // HelperConfig public helperConfig
 
-    address user = makeAddr("user");
+    address user1 = makeAddr("user1");
     address user2 = makeAddr("user2");
+    address public owner;
 
-    // uint256 startingUserBalance = ;
+    uint256 public startingUserBalance = 10 ether;
 
     function setUp() external {
-        dsc = new DecentralizedStableCoin();
+        dsc = new DecentralizedStableCoin(msg.sender);
+        owner = msg.sender;
+        vm.deal(user1, startingUserBalance);
+        vm.deal(user2, startingUserBalance);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                  MINT
+    //////////////////////////////////////////////////////////////*/
+
+    function testMintFailsWhenCalledByAnyoneOtherThanOwner() external {
+        uint256 amount = 1000;
+        vm.prank(user1);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Ownable.OwnableUnauthorizedAccount.selector,
+                address(user1)
+            )
+        );
+        dsc.mint(user1, amount);
+    }
+
+    function testMintFailsWhenToAddressIsZeroAddress() external {
+        uint256 amount = 1000;
+        vm.prank(owner);
+        vm.expectRevert(
+            DecentralizedStableCoin
+                .DecentralizedStableCoin__NotZeroAddress
+                .selector
+        );
+        dsc.mint(address(0), amount);
+    }
+
+    function testMintFailsWhenAmountIsZero() external {
+        uint256 amount = 0;
+        vm.prank(owner);
+        vm.expectRevert(
+            DecentralizedStableCoin
+                .DecentralizedStableCoin__AmountMustBeMoreThanZero
+                .selector
+        );
+        dsc.mint(owner, amount);
+    }
+
+    function testMintSucceedsWhenCalledByOwner() external {
+        uint256 amount = 1000;
+        vm.prank(owner);
+        bool success = dsc.mint(user1, amount);
+        assert(success);
+        assertEq(dsc.balanceOf(user1), amount);
+    }
+
+    modifier minted() {
+        uint256 amount = 1000;
+        vm.prank(owner);
+        bool success = dsc.mint(owner, amount);
+        assert(success);
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                  BURN
+    //////////////////////////////////////////////////////////////*/
+
+    function testBurnFailsWhenCalledByAnyoneOtherThanOwner() external {
+        uint256 amount = 1000;
+        vm.prank(user1);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Ownable.OwnableUnauthorizedAccount.selector,
+                address(user1)
+            )
+        );
+        dsc.burn(amount);
+    }
+
+    function testBurnFailsWhenAmountIsZero() external {
+        uint256 amount = 0;
+        vm.prank(owner);
+        vm.expectRevert(
+            DecentralizedStableCoin
+                .DecentralizedStableCoin__AmountMustBeMoreThanZero
+                .selector
+        );
+        dsc.burn(amount);
+    }
+
+    function testBurnFailsWhenAmountIsGreaterThanBalance() external minted {
+        uint256 amount = 1001;
+        vm.prank(owner);
+        vm.expectRevert(
+            DecentralizedStableCoin
+                .DecentralizedStableCoin__BurnAmountExceedsBalance
+                .selector
+        );
+        dsc.burn(amount);
+    }
+
+    function testBurnSucceedsWhenCalledByOwner() external minted {
+        uint256 amount = 1000;
+        assertEq(dsc.balanceOf(owner), amount);
+        vm.prank(owner);
+        dsc.burn(amount);
+        assertEq(dsc.balanceOf(owner), 0);
     }
 }

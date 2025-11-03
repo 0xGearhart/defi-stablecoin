27e33aa947afc1e7a2e74501ddf4e384a88044cf
gearhart <geardev26@gmail.com>
2025-11-03 11:46:07 -0800

    tests for deploy script & helper config

--- test/integration/DeployDSCTest.t.sol
+++ test/integration/DeployDSCTest.t.sol
@@ -1,46 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Test, console} from "forge-std/Test.sol";
-import {DeployDSC} from "../../script/DeployDSC.s.sol";
-import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {HelperConfig, CodeConstants} from "../../script/HelperConfig.s.sol";
+import {Test, console} from "forge-std/Test.sol";
 
-contract DecentralizedStableCoinTest is Test {
+contract DecentralizedStableCoinTest is Test, CodeConstants {
     DeployDSC public deployer;
+    DSCEngine public dscEngine;
     HelperConfig public helperConfig;
     DecentralizedStableCoin public dsc;
-    HelperConfig.NetworkConfig public config;
+    address public ethUsdPriceFeed;
+    address public btcUsdPriceFeed;
+    address public weth;
+    address public wbtc;
+    address public account;
 
     function setUp() external {
         deployer = new DeployDSC();
-        (dsc, helperConfig) = deployer.run();
-        config = helperConfig.getConfig();
+        (dsc, dscEngine, helperConfig) = deployer.run();
+        (ethUsdPriceFeed, btcUsdPriceFeed, weth, wbtc, account) = helperConfig.activeNetworkConfig();
     }
 
     /*//////////////////////////////////////////////////////////////
                            INITIAL DSC STATE
     //////////////////////////////////////////////////////////////*/
 
-    function testDscNameWasSetCorrectly() external {
-        assertEq(dsc.name(), );
+    function testDscNameWasSetCorrectly() external view {
+        assertEq(dsc.name(), DSC_NAME);
     }
 
-    function testDscSymbolWasSetCorrectly() external {
-        assertEq(dsc.symbol(), );
+    function testDscSymbolWasSetCorrectly() external view {
+        assertEq(dsc.symbol(), DSC_SYMBOL);
     }
 
-    function testDscOwnerWasSetCorrectly() external {}
+    function testDscOwnerWasSetCorrectly() external view {
+        assertEq(dsc.owner(), address(dscEngine));
+    }
 
     /*//////////////////////////////////////////////////////////////
                        INITIAL DSC ENGINE STATE
     //////////////////////////////////////////////////////////////*/
 
-    function testDscEngineWethUsdPriceFeedWasSetCorrectly() external {}
+    function testDscEngineEthUsdPriceFeedWasSetCorrectly() external view {
+        assertEq(dscEngine.getPriceFeedAddress(weth), ethUsdPriceFeed);
+        assert(dscEngine.getPriceFeedAddress(weth) != address(0));
+    }
 
-    function testDscEngineWbtcUsdPriceFeedWasSetCorrectly() external {}
-
-    function testDscEngineWethAddressWasSetCorrectly() external {}
-
-    function testDscEngineWbtcAddressWasSetCorrectly() external {}
+    function testDscEngineBtcUsdPriceFeedWasSetCorrectly() external view {
+        assertEq(dscEngine.getPriceFeedAddress(wbtc), btcUsdPriceFeed);
+        assert(dscEngine.getPriceFeedAddress(wbtc) != address(0));
+    }
 }

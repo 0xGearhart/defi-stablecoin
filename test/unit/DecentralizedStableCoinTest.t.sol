1ac85876775d134065436d42c4daa220cb703d39
gearhart <geardev26@gmail.com>
2025-11-07 15:34:33 -0800

    fix DSC tests

--- test/unit/DecentralizedStableCoinTest.t.sol
+++ test/unit/DecentralizedStableCoinTest.t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Test, console} from "forge-std/Test.sol";
 import {CodeConstants} from "../../script/HelperConfig.s.sol";
 import {DecentralizedStableCoin, Ownable} from "../../src/DecentralizedStableCoin.sol";
+import {Test, console} from "forge-std/Test.sol";
 
 contract DecentralizedStableCoinTest is Test, CodeConstants {
     DecentralizedStableCoin public dsc;

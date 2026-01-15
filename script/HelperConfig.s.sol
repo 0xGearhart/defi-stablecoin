ee03227076da79c0167ab6e0f34883a47397cccd
0xGearhart <geardev26@gmail.com>
2026-01-15 14:32:10 -0800

    add error for invalid chain ids and deployment support for arbitrum sepolia & mainnet

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
-import {Script} from "forge-std/Script.sol";
+import {Script, console2} from "forge-std/Script.sol";
 
 abstract contract CodeConstants {
     // DSC name and symbol
@@ -23,19 +23,18 @@ abstract contract CodeConstants {
     address public constant WBTC_ETH_SEPOLIA_ADDRESS = 0x7079A35DAAa3fEc63F52496CAbBFac0f9D5beB28;
     address public constant WBTC_ETH_SEPOLIA_PRICE_FEED_ADDRESS = 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43;
 
-    // ToDo: fill in all these ARB addresses marked address(0)
     // arb mainnet chain id and info
     uint256 public constant ARB_MAINNET_CHAIN_ID = 42_161;
-    address public constant WETH_ARB_MAINNET_ADDRESS = address(0);
-    address public constant WETH_ARB_MAINNET_PRICE_FEED_ADDRESS = address(0);
-    address public constant WBTC_ARB_MAINNET_ADDRESS = address(0);
-    address public constant WBTC_ARB_MAINNET_PRICE_FEED_ADDRESS = address(0);
+    address public constant WETH_ARB_MAINNET_ADDRESS = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+    address public constant WETH_ARB_MAINNET_PRICE_FEED_ADDRESS = 0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612;
+    address public constant WBTC_ARB_MAINNET_ADDRESS = 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f;
+    address public constant WBTC_ARB_MAINNET_PRICE_FEED_ADDRESS = 0xd0C7101eACbB49F3deCcCc166d238410D6D46d57;
     // arb sepolia chain id and info
     uint256 public constant ARB_SEPOLIA_CHAIN_ID = 421_614;
-    address public constant WETH_ARB_SEPOLIA_ADDRESS = address(0);
-    address public constant WETH_ARB_SEPOLIA_PRICE_FEED_ADDRESS = address(0);
-    address public constant WBTC_ARB_SEPOLIA_ADDRESS = address(0);
-    address public constant WBTC_ARB_SEPOLIA_PRICE_FEED_ADDRESS = address(0);
+    address public constant WETH_ARB_SEPOLIA_ADDRESS = 0x980B62Da83eFf3D4576C647993b0c1D7faf17c73;
+    address public constant WETH_ARB_SEPOLIA_PRICE_FEED_ADDRESS = 0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165;
+    address public constant WBTC_ARB_SEPOLIA_ADDRESS = 0x57A618Ab6e7abDdCD9A417F078d1958099DaC8b6;
+    address public constant WBTC_ARB_SEPOLIA_PRICE_FEED_ADDRESS = 0x56a43EB56Da12C0dc1D972ACb089c06a5dEF8e69;
 
     // local chain id and info
     uint256 public constant LOCAL_CHAIN_ID = 31_337;
@@ -47,6 +46,8 @@ abstract contract CodeConstants {
 }
 
 contract HelperConfig is Script, CodeConstants {
+    error HelperConfig__InvalidChainId();
+
     NetworkConfig public activeNetworkConfig;
 
     struct NetworkConfig {
@@ -66,8 +67,10 @@ contract HelperConfig is Script, CodeConstants {
             activeNetworkConfig = getArbMainnetConfig();
         } else if (block.chainid == ARB_SEPOLIA_CHAIN_ID) {
             activeNetworkConfig = getArbSepoliaConfig();
-        } else {
+        } else if (block.chainid == LOCAL_CHAIN_ID) {
             activeNetworkConfig = getOrCreateAnvilEthConfig();
+        } else {
+            revert HelperConfig__InvalidChainId();
         }
     }
 

fc4f210e98009733e99300d15dc6b2cc358e76fd
0xGearhart <geardev26@gmail.com>
2026-01-15 19:06:28 -0800

    simplify deployment function

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -11,14 +11,7 @@ contract DeployDSC is Script, CodeConstants {
     address[] public tokenAddresses;
     address[] public priceFeedAddresses;
 
-    function run() external returns (DecentralizedStableCoin, DSCEngine, HelperConfig) {
-        return deployContract();
-    }
-
-    function deployContract()
-        public
-        returns (DecentralizedStableCoin dsc, DSCEngine dscEngine, HelperConfig currentConfig)
-    {
+    function run() external returns (DecentralizedStableCoin dsc, DSCEngine dscEngine, HelperConfig currentConfig) {
         // get deploy info for current chainid
         currentConfig = new HelperConfig();
         (address ethUsdPriceFeed, address btcUsdPriceFeed, address weth, address wbtc, address account) =

22c405a9c26939feea510492471963337f796c55
0xGearhart <geardev26@gmail.com>
2026-01-15 16:41:39 -0800

    solidity version consistency with OZ lib

--- test/fuzz/failOnRevert/Handler.t.sol
+++ test/fuzz/failOnRevert/Handler.t.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.25;
+pragma solidity 0.8.20;
 
 import {AggregatorV3Interface, DSCEngine} from "../../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../../src/DecentralizedStableCoin.sol";
